d45be1f7b653530a37d210ea58c4b334d1ff1a2e
JonathanAdif <74348858+JonathanAdif@users.noreply.github.com>
2020-12-02 16:33:58 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+export default function App() {
+  const [timer, setTimer] = React.useState(60);
+  const id = React.useRef(null);
+  const clear = () => {
+    window.clearInterval(id.current);
+  };
+  React.useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((time) => time - 1);
+    }, 1000);
+    return () => clear();
+  }, []);
+
+  React.useEffect(() => {
+    if (timer === 0) {
+      clear();
+    }
+  }, [timer]);
+
+  return (
+    <div className="App">
+      <div>Time remaining : {timer} </div>
+    </div>
+  );
+}
